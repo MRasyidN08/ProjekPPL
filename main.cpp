c76aef5c81b6b647ce3d4ab5f608df5e0bdcddf5
Lyalmoon <rasyidn0812@gmail.com>
2024-11-06 00:52:32 +0700

    benerin bug pilihan diluar int

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 // main.cpp
 #include <iostream>
+#include <limits>
 #include "Heading.hpp"
 
 using namespace std;
 
 int jumlahItem;
 double totalHarga;
+
+int mintaPilihan() {
+    int pilihan;
+    while (true) {
+        if (cin >> pilihan) {
+            // Jika input valid (integer)
+            return pilihan;
+        } else {
+            // Jika input tidak valid
+            cin.clear(); // Menghapus status error dari cin
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Membuang input yang tidak valid
+            cout << "Input tidak valid, silakan masukkan angka." << endl;
+        }
+    }
+}
 
 void pegawai(MenuItem menu[], int ukuran, int pilihan) {
     if (pilihan > 0 && pilihan <= ukuran){
@@ -51,7 +67,7 @@ int main() {
         do {
             tampilkanMenu(menu, ukuran);
             cout << "Ingin menambah stok makanan apa? (masukkan nomor item, 0 untuk selesai):";
-            cin >> pilihan;
+            pilihan = mintaPilihan();
             pegawai(menu, ukuran, pilihan);
         } while (pilihan != 0);
         cout << "Terima kasih";
@@ -60,7 +76,8 @@ int main() {
         do {
             tampilkanMenu(menu, ukuran);
             cout << "Pilih item dari menu (masukkan nomor item, 0 untuk selesai): ";
-            cin >> pilihan;
+            pilihan = mintaPilihan();
+            
             if (pilihan > 0 && pilihan <= ukuran) {
                 customer(menu, ukuran, pilihan);
             } else {
