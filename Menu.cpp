0793e8e146c3e87aa1ecdcb3a8ebd12512007f84
kaffiahmad <kaffiahmad22@gmail.com>
2024-11-06 00:14:03 +0700

    asa

--- Menu.cpp
+++ Menu.cpp
@@ -31,14 +31,14 @@ bool prosesPembayaran(double totalHarga) {
 void isiMenu(MenuItem menu[], int& ukuran) {
     // Mengisi data menu
     menu[0] = {"Burger (Big mac)", 42000, menu[0].stok};
-    menu[1] = {"Cheeseburger", 35000};
-    menu[2] = {"Double Cheesburger", 41000};
-    menu[3] = {"Chicken Burger", 33000};
-    menu[4] = {"Chicken Burger", 33000};
-    menu[5] = {"Panas Special (Ayam,Nasi,Telur,Minum)", 47000};
-    menu[6] = {"Panas 1 (Ayam,Nasi,Minum)", 39000};
-    menu[7] = {"Pamer 5 (Ayam 5,Nasi 5,Minum 5)", 132500};
-    menu[8] = {"Pamer 7 (Ayam 7,Nasi 7,Minum 7)", 185000};
+    menu[1] = {"Cheeseburger", 35000, menu[1].stok};
+    menu[2] = {"Double Cheesburger", 41000, menu[2].stok};
+    menu[3] = {"Chicken Burger", 33000, menu[3].stok};
+    menu[4] = {"Chicken Burger", 33000, menu[4].stok};
+    menu[5] = {"Panas Special (Ayam,Nasi,Telur,Minum)", 47000, menu[5].stok};
+    menu[6] = {"Panas 1 (Ayam,Nasi,Minum)", 39000, menu[6].stok};
+    menu[7] = {"Pamer 5 (Ayam 5,Nasi 5,Minum 5)", 132500, menu[7].stok};
+    menu[8] = {"Pamer 7 (Ayam 7,Nasi 7,Minum 7)", 185000, menu[8].stok};
     ukuran = 9; // Mengatur ukuran menu
 }
 
